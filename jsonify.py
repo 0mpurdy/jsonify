47a3f674d3f94385da48255401ac41549b8d04ae
Michael Purdy <0mpurdy@gmail.com>
2021-04-06 20:34:50 +0100

    Support repeated jsonification

--- jsonify.py
+++ jsonify.py
@@ -2,8 +2,13 @@ import json
 import pyperclip
 
 text = input('Enter JSON string: ')
+
+if not text.startswith('"'):
+    text = '"' + text + '"'
+
 loaded = json.loads(text)
 mini = json.dumps(loaded, separators=(',', ':'))
+mini = mini.replace('\\', '\\\\')
 mini = mini.replace('"', '\\"')
 
 print(mini)
--- unjsonify.py
+++ unjsonify.py
@@ -2,6 +2,11 @@ import json
 import pyperclip
 
 text = input('Enter encoded JSON string: ')
+
+if text.startswith('"'):
+    text = text[1:-1]
+
+text = text.replace('\\\\', '\\')
 text = text.replace('\\"', '"')
 loaded = json.loads(text)
 
