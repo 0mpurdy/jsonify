ce35df589c77930cba57a11d9e79be8fd3b3fe30
Michael Purdy <0mpurdy@gmail.com>
2021-04-07 11:47:26 +0100

    Fix nested jsonify

--- jsonify.py
+++ jsonify.py
@@ -3,13 +3,11 @@ import pyperclip
 
 text = input('Enter JSON string: ')
 
-if not text.startswith('"'):
-    text = '"' + text + '"'
-
 loaded = json.loads(text)
 mini = json.dumps(loaded, separators=(',', ':'))
 mini = mini.replace('\\', '\\\\')
 mini = mini.replace('"', '\\"')
+mini = '"' + mini + '"'
 
 print(mini)
 pyperclip.copy(mini)
