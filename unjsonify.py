aa7c0390e3156c0c827d3a481f55c9366ac66917
Michael Purdy <0mpurdy@gmail.com>
2021-06-04 21:55:03 +0100

    When reading from input file, print to output file

--- unjsonify.py
+++ unjsonify.py
@@ -2,7 +2,9 @@ import json
 import pyperclip
 
 text = input('Enter encoded JSON string: ')
-if text == "":
+
+use_input_file = text == ""
+if use_input_file:
     with open('input.json') as f:
         text = f.read()
 
@@ -17,5 +19,10 @@ loaded = json.loads(text)
 
 pretty = json.dumps(loaded, indent = 4)
 
-print(pretty)
+if use_input_file:
+    with open('output.json', "w") as f:
+        print(pretty, file = f)
+else:
+    print(pretty)
+
 pyperclip.copy(pretty)
