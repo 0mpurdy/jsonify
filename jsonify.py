a2fa1d91adf0557114660ee4ab94d8b062b7da12
Michael Purdy <0mpurdy@gmail.com>
2021-06-21 21:44:32 +0100

    Use input and output files for jsonify

--- jsonify.py
+++ jsonify.py
@@ -3,11 +3,23 @@ import pyperclip
 
 text = input('Enter JSON string: ')
 
+use_input_file = text == ""
+if use_input_file:
+    with open('input.json') as f:
+        text = f.read()
+
+text = text.rstrip("\n")
+
 loaded = json.loads(text)
 mini = json.dumps(loaded, separators=(',', ':'))
 mini = mini.replace('\\', '\\\\')
 mini = mini.replace('"', '\\"')
 mini = '"' + mini + '"'
 
-print(mini)
+if use_input_file:
+    with open('output.json', "w") as f:
+        print(mini, file = f)
+else:
+    print(mini)
+
 pyperclip.copy(mini)
